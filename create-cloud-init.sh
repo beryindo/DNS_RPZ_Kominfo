863db044e3f0fac56be4bf2ee1a2f89c97af779a
Bery Indo <bery.indo@gmail.com>
2025-10-03 21:51:01 +0700

    Update create-cloud-init.sh

--- create-cloud-init.sh
+++ create-cloud-init.sh
@@ -3,7 +3,7 @@
 # License: GPL2
 
 wget https://cloud.debian.org/images/cloud/bookworm/latest/debian-12-generic-amd64.qcow2
-virt-customize -a debian-12-generic-amd64.qcow2 --install qemu-guest-agent --install resolvconf --install systemd-resolved --update --run-command 'mkdir -p /etc/network/interfaces.d' --run-command 'echo "auto >
+virt-customize -a debian-12-generic-amd64.qcow2 --install qemu-guest-agent --install resolvconf --install systemd-resolved --update --run-command 'mkdir -p /etc/network/interfaces.d' --run-command 'echo "auto ens18" >> /etc/network/interfaces.d/ens18' --run-command 'echo "iface ens18 inet manual" >> /etc/network/interfaces.d/ens18'
 
 qm create 100000 --name "debian12-cloudinit-template" --memory 2048 --net0 virtio,bridge=vmbr0
 qm importdisk 100000 debian-12-generic-amd64.qcow2 local-lvm -format qcow2
